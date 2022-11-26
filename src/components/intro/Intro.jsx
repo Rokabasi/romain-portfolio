bf68546b48aeee1bd2ddefc3e26d2163f4add8ea
Romain kabasi <kabasiromain@gmail.com>
2022-11-26 22:26:12 +0100

    style:fix the backgroung image of introduction section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,13 @@ import './Intro.css'
 import Github from "../../img/github.png"
 import LinkedIn from "../../img/linkedin.png"
 import Instagram from "../../img/instagram.png"
+import Vector1 from "../../img/Vector1.png"
+import Vector2 from "../../img/Vector2.png"
+import boy from "../../img/boy.png"
+import thumbup from "../../img/thumbup.png"
+import crown from "../../img/crown.png"
+import glassesimoji from '../..//img/glassesimoji.png'
+
 function Intro() {
   return (
     <div className='intro'>
@@ -31,7 +38,9 @@ function Intro() {
         </div>
         </div>
         <div className="i-right">
-            I am right side
+          <img src={Vector1} alt="" />
+          <img src={Vector2} alt="" />
+          <img src={boy} alt="" />
         </div>
     </div>
   )
