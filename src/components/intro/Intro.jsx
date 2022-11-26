850d013d2183eca3418b8cf5ba9573704cc80f83
Romain kabasi <kabasiromain@gmail.com>
2022-11-26 21:19:00 +0100

    feat:add introduction section

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Intro.css'
+function Intro() {
+  return (
+    <div className='intro'>
+        <div className="i-left">
+        <div className="i-name">
+            <span>Hy! I am</span>
+            <span>Romain KABASI</span>
+            <span>Fullstack developper with high level of experience in web designing and development, producting the Quality work
+            </span>
+        </div>
+        <button className="button i-button">
+            Hire me
+        </button>
+        </div>
+        <div className="i-right">
+            I am right side
+        </div>
+    </div>
+  )
+}
+
+export default Intro
