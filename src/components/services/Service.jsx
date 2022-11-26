5ffe617eec5eb1e4fdafe7d32f52583894d8f10c
Romain kabasi <kabasiromain@gmail.com>
2022-11-26 23:34:37 +0100

    feat:add services folder

---
+++ src/components/services/Service.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import 'Services.css'
+import HeartEmoji from '../../img/heartemoji.png'
+import Glasses from '../../img/glasses.png'
+
+
+function Services () {
+  return (
+    <div className='services'>
+
+        <div className='awesome'>
+
+        </div>
+        <div className='cards'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Services
